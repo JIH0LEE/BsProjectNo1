d78496afd3049af15f925bb05e587fe78e57f41b
JIH0LEE <skql5810@gmail.com>
2021-03-17 21:59:46 +0900

    Jiho/Add Portriats

--- BaekSeokGame/Assets/Jiho/Script/DialogController.cs
+++ BaekSeokGame/Assets/Jiho/Script/DialogController.cs
@@ -8,28 +8,33 @@ public class DialogController : MonoBehaviour
 
 
  {
+
     public GameObject hidingObject;
     public GameObject dialogUI;
     public Text npcName;
     public Text dialogBody;
     public DialogData dialogData;
+    public GameObject portraitSprite;
+   
     public int dialogIdx=0;
     bool isTalking;
-   
+    Image portriatImage;
     void Start()
     {
         dialogIdx = 0;
-        
-    }
+
+        portriatImage = portraitSprite.GetComponent<Image>();
+    }       
 
     void Update()
     {
-            
+        
     }
 
     public void Action(GameObject npc)
     {
         ObjectData objData = npc.GetComponent<ObjectData>();
+        
         Talk(objData);
 
         //Change ui status through isTalking flag.
@@ -38,8 +43,8 @@ public class DialogController : MonoBehaviour
     }
     void Talk(ObjectData npc)
     {
-
-        string nameText = npc.name;
+        
+        string nameText = npc.npcName;
         string body = dialogData.GetDialog(npc.id, dialogIdx);
         if (body == null)
         {
@@ -49,8 +54,9 @@ public class DialogController : MonoBehaviour
         }
         else
         {
-            npcName.text = npc.name;
-            dialogBody.text = dialogData.GetDialog(npc.id, dialogIdx);
+            npcName.text =nameText;
+            dialogBody.text = body.Split(':')[0];
+            portriatImage.sprite = dialogData.GetPortrait(npc.id,int.Parse(body.Split(':')[1]));
             isTalking = true;
             dialogIdx++;
         }
--- BaekSeokGame/Assets/Jiho/Script/DialogData.cs
+++ BaekSeokGame/Assets/Jiho/Script/DialogData.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class DialogData : MonoBehaviour
 {
-
+    Dictionary<int, Sprite> portraitData;
     Dictionary<int, string[]> dialogData;
+    public Sprite[] portraits;
         void Start()
     {
         dialogData = new Dictionary<int, string[]>();
+        portraitData = new Dictionary<int, Sprite>();
         GenerateData();
     }
     void GenerateData()
     {
-        dialogData.Add(1000, new string[] { "어서오시게, 이 세계에...",
-                                            "내가 누구냐고? 하하! 성미가 급한 친구구만.", 
-                                            "이 세계에서 여행을 하다보면 언젠간 알게 되겠지."});
-        dialogData.Add(1100, new string[] {  "당신인가. Q의 의지를 잇는 자가...",
-                                            "이 세계를 바꿔주게...신들은 인계에 직접 관여를 할 수 없기에..."});
+        dialogData.Add(1000, new string[] { "어서오시게, 이 세 계에...:0",
+                                            "내가 누구냐고? 하하! 성미가 급한 친구구만.:1", 
+                                            "이 세계에서 여행을 하다보면 언젠간 알게 되겠지.:0"});
+        dialogData.Add(1100, new string[] { "당신인가. Q의 의지를 잇는 자가...:0",
+                                            "이 세계를 바꿔주게...신들은 인계에 직접 관여를 할 수 없기에...:1"});
+        portraitData.Add(1000 + 0, portraits[0]);
+        portraitData.Add(1000 + 1, portraits[1]);
+        portraitData.Add(1100 + 0, portraits[0]);
+        portraitData.Add(1100 + 1, portraits[1]);
+        
     }
 
     public string GetDialog(int id,int idx)
@@ -31,6 +38,10 @@ public class DialogData : MonoBehaviour
             return dialogData[id][idx];
         }
     }
+    public Sprite GetPortrait(int id, int idx)
+    {
+        return portraitData[id+idx];
+    }
 
-    
+
 }
