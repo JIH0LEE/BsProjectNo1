57041b60b310afd25692dafcb71c39f6bbd8f018
2016025632 <dbejrrb0912@naver.com>
2021-03-21 17:49:07 +0900

    이거다!

---
+++ BaekSeokGame/Assets/Jiho/Script/Player_Click.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Player_Click : MonoBehaviour
+{
+
+    bool isPlayerOnPortal;
+
+    bool isPlayerOnNPC;
+
+    string transferMapName;
+
+    string talkNPCName;
+
+    public Portal portal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlayerOnPortal = false;
+        isPlayerOnNPC = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D Object)
+    {
+
+        Debug.Log(Object.gameObject);
+
+        if (Object.tag == "portals")
+        {
+
+            isPlayerOnPortal = true;
+            portal = Object.gameObject.GetComponent<Portal>();
+        }
+        if (Object.tag == "npc")
+        {
+            isPlayerOnNPC = true;
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D Object)
+    {
+
+        if (Object.tag == "portals")
+        {
+            isPlayerOnPortal = false;
+            portal = null;
+        }
+        if (Object.tag == "npc")
+        {
+            isPlayerOnNPC = false;
+        }
+
+    }
+    public void Click()
+    {
+        Debug.Log("click");
+        if (isPlayerOnPortal == true)
+        {
+            Portal();
+        }
+        if (isPlayerOnNPC == true)
+        {
+            Talk();
+        }
+    }
+
+    public void Portal()
+    {
+        Debug.Log("portal click");
+        Debug.Log(portal.nextMap);
+        SceneManager.LoadScene(portal.nextMap);
+        this.transform.position = portal.nextPosition;
+    }
+
+    public void Talk()
+    {
+        Debug.Log("npc click");
+    }
+
+}
--- BaekSeokGame/Assets/Jiho/Script/Portal.cs
+++ BaekSeokGame/Assets/Jiho/Script/Portal.cs
@@ -1,77 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class Portal : MonoBehaviour
 {
     
     public string currentMap;
     public string nextMap;
-    
-    bool isClicked;
+    public Vector2 nextPosition;
 
-    PlayerController playerControl;
-    GameObject player;
-    GameObject okButton;
-    PointerListener clickOk;
-    bool isPlayerOn = false;
-    
-        void Start()
-    {
-        isClicked = false;
-        okButton = GameObject.Find("OKButton");
-        playerControl = FindObjectOfType<PlayerController>();
-        clickOk = okButton.GetComponent<PointerListener>();   
-            player = GameObject.FindGameObjectWithTag("Player");
-        if (playerControl.currentMapName == currentMap)
-        {
-            player.transform.position = this.transform.position;
-        }
-    }
-    public void isClickedChange()
-    {
-        if (isPlayerOn)
-        {
-
-            isClicked = true;
-        }
-    }
-
-    
-    void Update()
-    {
-        if (isClicked && isPlayerOn)
-        {
-
-            playerControl.currentMapName = nextMap;
-            isClicked = false;
-            SceneManager.LoadScene(nextMap);
-        }
-                   
-        
-    }
-
-    void OnTriggerEnter2D(Collider2D Object)
-    {
-        if (Object.tag == "Player")
-        {
-            
-            isPlayerOn = true;
-           
-        }
-
-    }
-    void OnTriggerExit2D(Collider2D Object)
-    {
-        if (Object.tag == "Player")
-        {
-           
-            isPlayerOn = false;
-           
-        }
-
-      
-
-    }
 }
